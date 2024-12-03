7e9948735f05c28e7ff5fc6628e0fe2bd918cf6a
Sam Widger <widge33@gmail.com>
2024-12-03 11:29:57 +0000

    :santa: day 1

--- index.js
+++ index.js
@@ -1,3 +1,4 @@
-import { task1 } from './src/day1/index.js'
+import { task1, task2 } from './src/day1/index.js'
 
-console.log("Task 1 Output: "+task1());
+console.log(task1());
+console.log(task2());christmas_tree
--- src/day1/index.js
+++ src/day1/index.js
@@ -1,3 +1,44 @@
+import {convertInputFileToString} from "../utils/utils.js";
+
+//read input file and convert to an array of each line
+const listsData = convertInputFileToString("src/day1/input.txt")
+  .split("\n");
+
+const list1 = [];
+const list2 = [];
+
+// Q.A.D. - break each line out into two lists based on spacing between numbers.
+// Haven't got time to make the whitespace split more elegant
+listsData.forEach((listLine) => {
+  if (listLine === "") {
+    return;
+  }
+  list1.push(parseInt(listLine.split("   ")[0]));
+  list2.push(parseInt(listLine.split("   ")[1]));
+});
+
 export const task1 = () => {
-  return "Hello World"
+
+  list1.sort();
+  list2.sort();
+  let total = 0;
+
+  for(let i = 0; i < list1.length; i++) {
+    total += Math.abs(list1[i] - list2[i]);
+  }
+
+  return `Total is: ${total}`;
+}
+
+export const task2 = () => {
+
+  let total = 0;
+
+  for(let id of list1) {
+    // Use reduce to efficiently count how many times the id appears in list2
+    const appearances = list2.reduce((count, current) => count + (current === id), 0);
+    total +=  (id * appearances);// Similarity Score
+  }
+
+  return `Combined Similarity Score is: ${total}`;
 }
